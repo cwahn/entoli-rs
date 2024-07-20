fb4a9ccf6c3bd7f94a8ad2fc6c2178983533addd
Chanwoo Ahn <cwahn@arsvivendi.io>
2024-07-21 00:06:06 +0900

    chore: Make benchmark more heavior with possitive obervation result for zero-cost-ness

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -2,12 +2,15 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use entoli::base::misc::in_place;
 
 fn benchmark_in_place(c: &mut Criterion) {
-    // Case 1: Allocation-related case
+    // Generate a large vector
+    let large_vec: Vec<i32> = (0..10_000).collect();
+
+    // Heavier computation for allocation-related case
     c.bench_function("manual_in_place (allocation)", |b| {
         let f = |v: &mut Vec<i32>| {
-            v.iter_mut().for_each(|x| *x *= 2);
+            v.iter_mut().for_each(|x| *x = *x * 2 + 3 - 1 * 5 / 2);
         };
-        let mut data = vec![1, 2, 3, 4, 5];
+        let mut data = large_vec.clone();
 
         b.iter(|| {
             f(&mut data);
@@ -16,8 +19,8 @@ fn benchmark_in_place(c: &mut Criterion) {
     });
 
     c.bench_function("in_place (allocation)", |b| {
-        let f = |v: Vec<i32>| -> Vec<i32> { v.into_iter().map(|x| x * 2).collect() };
-        let mut data = vec![1, 2, 3, 4, 5];
+        let f = |v: Vec<i32>| -> Vec<i32> { v.into_iter().map(|x| x * 2 + 3 - 1 * 5 / 2).collect() };
+        let mut data = large_vec.clone();
         let in_place_f = in_place(f);
 
         b.iter(|| {
@@ -27,8 +30,8 @@ fn benchmark_in_place(c: &mut Criterion) {
     });
 
     c.bench_function("non_in_place (allocation)", |b| {
-        let f = |v: Vec<i32>| -> Vec<i32> { v.into_iter().map(|x| x * 2).collect() };
-        let data = vec![1, 2, 3, 4, 5];
+        let f = |v: Vec<i32>| -> Vec<i32> { v.into_iter().map(|x| x * 2 + 3 - 1 * 5 / 2).collect() };
+        let data = large_vec.clone();
 
         b.iter(|| {
             let new_data = f(data.clone());
@@ -36,10 +39,10 @@ fn benchmark_in_place(c: &mut Criterion) {
         });
     });
 
-    // Case 2: Non-allocation-related case
+    // Heavier computation for non-allocation-related case
     c.bench_function("manual_in_place (non-allocation)", |b| {
         let f = |v: &mut i32| {
-            *v *= 2;
+            *v = *v * 2 + 3 - 1 * 5 / 2;
         };
         let mut data = 5;
 
@@ -50,7 +53,7 @@ fn benchmark_in_place(c: &mut Criterion) {
     });
 
     c.bench_function("in_place (non-allocation)", |b| {
-        let f = |v: i32| -> i32 { v * 2 };
+        let f = |v: i32| -> i32 { v * 2 + 3 - 1 * 5 / 2 };
         let mut data = 5;
         let in_place_f = in_place(f);
 
@@ -61,7 +64,7 @@ fn benchmark_in_place(c: &mut Criterion) {
     });
 
     c.bench_function("non_in_place (non-allocation)", |b| {
-        let f = |v: i32| -> i32 { v * 2 };
+        let f = |v: i32| -> i32 { v * 2 + 3 - 1 * 5 / 2 };
         let data = 5;
 
         b.iter(|| {
