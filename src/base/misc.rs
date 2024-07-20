36f1355618834475b5219a8f6e44d397cb30c9f6
Chanwoo Ahn <cwahn@arsvivendi.io>
2024-07-21 00:07:59 +0900

    fix: Fix in_place with benchmark

--- src/base/misc.rs
+++ src/base/misc.rs
@@ -1,18 +1,3 @@
-/// Return in-place mutating function from a homogeneous consuming function.
-///
-/// (a -> a) -> (&mut a -> ())
-/// Input function should not panic, otherwise UB.
-
-// #[inline(always)]
-// pub fn in_place<A, F>(f: F) -> impl Fn(&mut A) -> ()
-// where
-//     F: Fn(A) -> A,
-// {
-//     move |value: &mut A| unsafe {
-//         *value = f(std::ptr::read(value));
-//     }
-// }
-
 #[inline(always)]
 pub fn in_place<A, F>(f: F) -> impl Fn(&mut A) -> ()
 where
